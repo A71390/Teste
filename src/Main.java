b41a64401292d062167a7600a8521f4b6a4864ba
Bruno Nunes <a71390@ualg.pt>
2023-04-19 20:26:24 +0100

    Add new Class Ponto and did a test on the main file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Update");
-        //hbdsojdalga
-        System.out.println("Update 2");
+        Ponto p = new Ponto( 1, 2);
+        p.printPonto();
     }
 }
---
+++ src/Ponto.java
@@ -0,0 +1,12 @@
+public class Ponto {
+    int x, y;
+
+    public Ponto( int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void printPonto(){
+        System.out.println("( "+this.x+", "+this.y+")");
+    }
+}
